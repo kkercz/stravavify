c969096ee3ae0b0168458a922410d95b24ccf495
Krzysztof Kercz <k.kercz@gmail.com>
2025-06-28 00:00:26 +0200

    make output more readable

--- src/main/java/io/github/kkercz/stravavify/Main.java
+++ src/main/java/io/github/kkercz/stravavify/Main.java
@@ -4,16 +4,21 @@ import io.github.kkercz.stravavify.connector.ConnectorFactory;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("\n\n---------- Stravavify: START ----------\n\n\n");
+
         try {
             ConnectorFactory connectorFactory = new ConnectorFactory();
             Stravavify stravavify = new Stravavify(
                     connectorFactory.spotifyConnector(),
                     connectorFactory.stravaConnector());
 
-            stravavify.updateDescriptionWithSongs();
 
+            stravavify.updateDescriptionWithSongs();
         } catch (Exception ex) {
             ex.printStackTrace(System.err);
         }
+
+        System.out.println("\n\n\n----------- Stravavify: END -----------\n");
     }
 }
